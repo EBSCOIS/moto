9c25a0d2e96eb34ecf95fa9100e4093fca41cdb9
timski <tklopotoski@ebsco.com>
2021-08-11 17:43:22 -0400

    Apply suggestions from black

--- tests/test_events/test_events_cloudformation.py
+++ tests/test_events/test_events_cloudformation.py
@@ -45,9 +45,7 @@ rule_template = Template(
                     "Type": "AWS::Events::Rule",
                     "Properties": {
                         "Name": "${rule_name}",
-                        "EventPattern": {
-                            "detail-type": ["meteor"]
-                        },
+                        "EventPattern": {"detail-type": ["meteor"]},
                     },
                 }
             },
@@ -155,5 +153,3 @@ def test_create_rule():
     response = events_client.describe_rule(Name=name)
 
     response["Arn"].should.equal(rule_arn)
-
-
